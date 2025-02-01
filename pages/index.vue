4eb9b84a51398e963fa8430fb50f5d35be9ccf20
Koras02 <wjdgh0727@gmail.com>
2025-02-02 02:45:20 +0900

    finish anime site trailer add

---
+++ pages/index.vue
@@ -0,0 +1,100 @@
+<template>
+    <div>
+         <div class="header">
+            <Navigation />
+         </div>
+         <div class=" max-w-5xl shadow-md m-auto min-h-screen pt-3">
+            <div class=" px-2">
+              <div class="topAnime">
+                <div class=" h-10 w-full bg-green-900">
+                    <h1 class="text-2xl px4 text-white font-bold">최신 애니:</h1>
+                </div>
+                <TopAnime :topAnime="topAnime" />
+              </div>
+              <div>
+                <div class=" h-10 w-full bg-green-900">
+                   <h1 class="text-2xl px-4 text-white font-bold">Current Season:</h1>
+                </div>
+                <div class="currentSeason flex flex-wrap justify-center">
+                     <Card v-for="anime in response" :key="anime.mal_id" class="mx-auto" @click="goAnime(anime)">
+                         <template #image>
+                             <img :src="anime.images.webp.image_url" height="266" alt="anime.title"/>
+                         </template>
+                         <template #content>
+                            <h2 class=" font-bold">{{ anime.title }}</h2>
+                         </template>
+                         <template #episode>
+                           <p>Episodes: {{ anime.episodes  }}</p>
+                         </template>
+                     </Card>
+                </div>
+              </div>
+            </div>
+            <div class=" px-2">
+               <div class=" h-10 w-full bg-green-900">
+                 <h1 class="text-2x1 px-4 text-white font-bold">Anime Movie</h1>
+               </div>
+               <div class="animeMovie flex flex-wrap justify-center">
+                 <Card v-for="anime in response" :key="anime.mal_id" class="mx-auto" @click="goAnime(anime)">
+                  <template #image>
+                             <img :src="anime.images.webp.image_url" height="266" alt="anime.title"/>
+                         </template>
+                         <template #content>
+                            <h2 class=" font-bold">{{ anime.title }}</h2>
+                         </template>
+                         <template #episode>
+                           <p>Episodes: {{ anime.episodes  }}</p>
+                         </template>
+                 </Card>
+               </div>
+            </div>
+         </div>
+    </div>
+</template>
+
+<script setup>
+
+const topAnime = ref(null);
+const response = ref([]);
+const Movie = ref(null);
+
+onBeforeMount(() => {
+  getData()
+})
+
+onMounted(() => {
+  getData()
+})
+
+async function getData() {
+        try {
+            const {data, error} = await useFetch('https://api.jikan.moe/v4/seasons/now')
+            if(data.value) {
+              response.value = data.value.data
+            }
+
+             const { data: result } = await useLazyFetch('https://api.jikan.moe/v4/top/anime');
+             if(result.value) {
+                topAnime.value = result.value.data
+             }
+
+             const { data: refs } = await useLazyFetch(`https://api.jikan.moe/v4/top/anime?type=movie`);
+             if(result.value) {
+                 Movie.value = result.value.data
+             }
+        } catch(error) {
+            console.log(error)
+        }
+    }
+    
+  const router = useRouter();
+  const goAnime = (anime) => {
+    router.push(`/anime/${anime.mal_id}`)
+  }
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
